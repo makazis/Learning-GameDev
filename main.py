22161270b2e35f0286c97132ac6b943f3a1a5527
makazis <labdiennabagi@gmail.com>
2024-11-17 17:46:09 +0200

    c

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import pygame
 from math import *
 from random import *
+class Card:
+    def __init__(self): #Will be called when a new card is created
+        self.sides={
+            "Front":pygame.Surface((210,320)),
+            "Back":pygame.Surface((210,320))
+        } #2 sides for each card right now, maybe i'll add more later
 
 print("HIIIi")
