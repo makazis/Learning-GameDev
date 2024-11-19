c6cac7539985745cd23eda08bfe5f11098b14884
makazis <labdiennabagi@gmail.com>
2024-11-19 19:06:22 +0200

    idk what i did

--- player.py
+++ player.py
@@ -5,4 +5,6 @@ class Player:
     def __init__(self):
         self.health=100
         self.max_health=100
-        
+        self.resources={
+            "Mana":0 # Max mana increases by 1 every turn up to 6. It can be increased by other things such as creatures or spells, or relics later (spooked)
+        }
