61006b09b43757c448c00e24682dc9f74afa470a
makazis <labdiennabagi@gmail.com>
2024-11-23 16:31:59 +0200

    Added some basic board functionality, i hope

--- hand.py
+++ hand.py
@@ -9,17 +9,52 @@ class Card_Space:
             self.sprite.fill(card_transparency_color)
             pygame.draw.rect(self.sprite,(155,255,255),(0,0,210,320),10,15)
         else:
-            center(self.card.sprite,self.sprite,0,0)
+            self.card.draw()
+            center(self.card.sprite,self.sprite,105,160)
 class Board:
     def __init__(self):
         self.locations={  #Contains all the data about where cards can exist
             "Board":[]
         }
-        self.camera_x=0
-        self.camera_y=0
-    def add_space_to_board(self,x_offset=0,y_offset=0):
+        self.surface=pygame.Surface((1920,1080))
+        self.camera_x=-1920/2 
+        self.camera_y=-1080/2 #Aligns the center to the center
+    def add_space_to_board(self,x_offset=0,y_offset=0): #Adds a card spot to the board
         self.locations["Board"].append({
             "Space":Card_Space(),
             "X Offset":x_offset,
             "Y Offset":y_offset
             })
+    def setup_hand(self,max_cards=10):
+        self.locations["Hand"]={
+            "Position":(0,1000), #Can be changed later if needed
+            "Max Cards":max_cards,
+            "Cards":[],
+            "Curvature Settings":{ #Defines some stuff about how cards are held in hand, It's best not to think about this that much right now
+                "Alpha":10, #How much the cards are bent
+                "Beta":1.4, #Exponential of how far down the cards go, it's best to be kept small at larger number of cards
+                "Gamma":5 #Card Descent Multiplier in pixels
+            }
+        }
+    def draw(self):
+        self.surface.fill((5,5,45)) #Fills the board with a nice color to draw on
+        #Draws the board at the very bottom
+        for I in self.locations["Board"]:
+            #I=self.locations["Board"][i]
+            I["Space"].draw()
+            center(I["Space"].sprite,self.surface,I["X Offset"]-self.camera_x,I["Y Offset"]-self.camera_y)
+        if "Hand" in self.locations:
+            self.cards_in_hand=len(self.locations["Hand"]["Cards"])
+            curvature_settings=self.locations["Hand"]["Curvature Settings"]
+            for iterated_card in self.locations["Hand"]["Cards"]:
+                current_x=self.locations["Hand"]["Position"][0]-(self.cards_in_hand-1)/2*170 #Determines card position in hand
+                current_y=self.locations["Hand"]["Position"][1]
+                rotation=curvature_settings["Alpha"]
+                center(iterated_card.sprite,self.surface,current_x,current_y)
+    def update(self): #Updates the board so that 
+        self.mouse_rel=pygame.mouse.get_rel()
+        self.mouse_down=pygame.mouse.get_pressed()
+        if self.mouse_down[0]:
+            self.camera_x-=self.mouse_rel[0]
+            self.camera_y-=self.mouse_rel[1]
+            
--- main.py
+++ main.py
@@ -6,9 +6,19 @@ from hand import *
 pygame.init()
 win=pygame.display.set_mode((0,0)) #Sets the screen to fullscreen mode
 run=True
-cards=[Card() for i in range(5)]
+#cards=[Card() for i in range(5)]
 #new_card=Card()
 #new_card.side_from_surface(pygame.image.load("Resources/Sprites/Blue Eyes White Dragon.jpg"),"BEWD")
+board=Board()
+for i in range(5):
+    i-=2
+    for ii in range(4):
+        ii-=1.5
+        ii*=(1+abs(ii)/20)
+        board.add_space_to_board(i*220,ii*330)
+board.setup_hand()
+for i in range(5):
+    board.hand.append(Card())
 frame=0
 clock=pygame.time.Clock()
 while run:
@@ -18,11 +28,14 @@ while run:
         if event.type==pygame.QUIT:
             run=False
     win.fill((55,55,55)) #Deletes the screen, fills all with black
+    board.draw()
+    board.update()
     #for I,i in enumerate(cards):
     #    i.draw()
     #    if frame%200==I*20:
     #        i.flip(100)
     #    center(pygame.transform.rotate(i.sprite,(2-I)*10),win,200+I*130,300+abs(2-I)**2*20)
+    win.blit(pygame.transform.scale(board.surface,win.get_size()),(0,0))
     frame+=1
     pygame.display.update() #Updates the screen
 pygame.quit()
